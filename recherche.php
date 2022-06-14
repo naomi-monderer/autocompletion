4d5ae8a1d75a6ddfe23250976bb57f0e71ade7a9
naomie-monderer <92308722+naomie-monderer@users.noreply.github.com>
2022-06-14 14:50:38 +0200

    terminé css clean

--- recherche.php
+++ recherche.php
@@ -1,19 +1,9 @@
 <?php
 require_once('Artistes.php');
-
 $word = $_GET['searchBar'];
-
-
 $searchModel = new Artistes;
 $searchContains = $searchModel->search($word);
 $searchStarts = $searchModel->searchStart($word);
-
-// $artistesData = $searchModel->getById($id);
-// echo '<pre>';
-
-// var_dump($artistesData);
-// echo '</pre>';
-
 
 ?>
 <!DOCTYPE html>
@@ -33,8 +23,13 @@ $searchStarts = $searchModel->searchStart($word);
 <body>
 <header>
         <article class="parent-title">
-            <h1 class="h1-header">Gwokanng </h1>
-            <img class="star" src="star3.svg" alt="">
+            <a class="no-sublink" href="index.php"><h1 class="h1-header">Gwokanng </h1></a>
+            <!-- <img class="star" src="Star3.svg" alt=""> -->
+            <svg class="star" width="83" height="18" viewBox="0 0 83 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M9.30205 0.772748L11.498 7.33561H18.6041L12.8551 11.3917L15.051 17.9546L9.30205 13.8985L3.55307 17.9546L5.74898 11.3917L0 7.33561H7.10614L9.30205 0.772748Z" fill="white"/>
+                <path d="M40.1011 0.772736L42.297 7.3356H49.4032L43.6542 11.3917L45.8501 17.9545L40.1011 13.8985L34.3521 17.9545L36.548 11.3917L30.799 7.3356H37.9052L40.1011 0.772736Z" fill="white"/>
+                <path d="M73.3557 0.772736L75.5516 7.3356H82.6577L76.9087 11.3917L79.1047 17.9545L73.3557 13.8985L67.6067 17.9545L69.8026 11.3917L64.0536 7.3356H71.1598L73.3557 0.772736Z" fill="white"/>
+            </svg>
         </article>
         <section class="parent-search-bar">
             <article>
@@ -134,17 +129,13 @@ if(isset($_GET['display_artiste']))
     ?>
    
     <?php
-
-    var_dump($getById);
-    
-    // header('Location: element.php');
-
 }
+
 if(isset($_GET['display_all_searches']))
 {
     $id = $_GET['hidden_id_all_searches'];
     $getById = $searchModel->getById($id);
-    // header('Location: element.php');
+  
 }
  ?>
        
